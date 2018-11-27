3acf7a40f958f0fe614737c0968390b4d3fe8999
huguesfils <h.filscaparos@gmail.com>
2018-11-27 10:45:46 +0100

    Created class
    Class were created, Game, Player, Hero, and Hero types

--- DungeonBattle/DungeonBattle/main.swift
+++ DungeonBattle/DungeonBattle/main.swift
@@ -8,3 +8,50 @@
 
 // creation du fichier xcode
 
+class Game {
+    var player1: String
+    var player2: String
+    var playerTurn: Int
+    
+    init(player1: String, player2: String, playerTurn: Int) {
+        self.player1 = player1
+        self.player2 = player2
+        self.playerTurn = playerTurn
+    }
+}
+
+class Player {
+    var name: String
+    var team = [String]()
+    init (name: String){
+        self.name = name
+    }
+}
+
+class Hero {
+    var name: String
+    var life: Int
+    var weapon: String
+    
+    init(name: String, life: Int, weapon: String){
+        self.name = name
+        self.life = life
+        self.weapon = weapon
+    }
+}
+
+class Wizard: Hero {
+    
+}
+
+class Warrior: Hero {
+    
+}
+
+class Colossus: Hero {
+    
+}
+
+class Dwarf: Hero {
+    
+}
