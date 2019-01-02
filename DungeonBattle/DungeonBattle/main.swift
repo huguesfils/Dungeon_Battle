fc7c80842a0eda290547a6da2f0ffdc16e9470e4
huguesfils <h.filscaparos@gmail.com>
2019-01-02 22:09:59 +0100

    Working on the func fight(), creating the fighter team list

--- DungeonBattle/DungeonBattle/main.swift
+++ DungeonBattle/DungeonBattle/main.swift
@@ -35,33 +35,56 @@ class Game {
     }
     
     func fight(){
-        var turn = 0
-        print ("Tour \(turn)")
-        if let playerChoice = readLine(){
-            if let choice = Int(playerChoice){
-                switch choice {
-                case 1:
-                    print ("\(Player2!.team)")
-                    let Hero in player1.team =
-                case 2:
-                    
-                    
-                default:
-                    <#code#>
-                }
+        print ("""
+            Veuillez choisir avec quel personnage vous voulez combatre ce tour (1,2 ou 3)
+            1: \(String(describing: player1?.team[0]))
+            2: \(String(describing: player1?.team[1]))
+            3: \(String(describing: player1?.team[2]))
+            """)
+        for _ in 0..<player1!.team.count{
+            let fighter = readLine()
+            switch fighter{
+            case "1" :
+                print ("Vous avez choisi \(player1!.team[0])")
+            case "2":
+                print("Vous avez choisi \(player1!.team[1])")
+            case "3":
+                print("Vous avez choisi \(player1!.team[2])")
+            default:
+                print("Je ne comprends pas")
+                
             }
             
-            
-            //       choix perso equipe joueur1 et 2, effet .weapon - health hero adverse
-            //        turn +1 fin tour joueur (ah$jout else end avec mage solo ou team is empty
-            
         }
+        
+        /*var turn = 0
+         print ("Tour \(turn)")
+         if let playerChoice = readLine(){
+         if let choice = Int(playerChoice){
+         switch choice {
+         case 1:
+         print ("\(Player2!.team)")
+         let Hero in player1.team =
+         case 2:
+         
+         
+         default:
+         <#code#>
+         }
+         }
+         
+         
+         //       choix perso equipe joueur1 et 2, effet .weapon - health hero adverse
+         //        turn +1 fin tour joueur (ah$jout else end avec mage solo ou team is empty
+         
+         }*/
     }
 }
 
 let game = Game(playerTurn: 1)
 
-game.startGame()
+
+game.fight()
 
 /*creer le combat au tour par tour
  choisir un perso de l'équipe, puis choisir un adversaire (eneleve point de vie en fonction de l'arme)
@@ -69,6 +92,6 @@ game.startGame()
  changement tour
  idem tour 1
  si reste personne ou mage solo fin du jeu (
-*/
+ */
 
 
